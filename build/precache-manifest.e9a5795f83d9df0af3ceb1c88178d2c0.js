8083bef8edc1a13c6175473e37efcef1d945cdac
Louie <louie.gabriel@gmail.com>
2020-06-24 21:34:33 +0100

    see what happens if we add build...

---
+++ build/precache-manifest.e9a5795f83d9df0af3ceb1c88178d2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b0a36f1ea89f3dd808e495acd37987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23a0fc5db7374d7d7381",
+    "url": "/static/css/main.5b2c929b.chunk.css"
+  },
+  {
+    "revision": "33a8d5b38d24ab218183",
+    "url": "/static/js/2.f04ed9e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f04ed9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a0fc5db7374d7d7381",
+    "url": "/static/js/main.3d007bfe.chunk.js"
+  },
+  {
+    "revision": "84b8978c7bc4b4cb5368",
+    "url": "/static/js/runtime-main.2a50f9bc.js"
+  }
+]);
